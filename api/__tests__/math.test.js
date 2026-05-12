24603db38cfd63c61800a5bd0f0ccdf00d8593bb
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2026-05-12 11:23:29 +0400

    CI/CD api run test

---
+++ api/__tests__/math.test.js
@@ -0,0 +1,11 @@
+function division(a, b){
+    return a/b;
+}
+
+test("10/2 equal 5",()=>{
+    expect(division(10, 2)).toBe(5)
+})
+
+test("15/5 equal 3",()=>{
+    expect(division(15, 5)).toBe(3)
+})
