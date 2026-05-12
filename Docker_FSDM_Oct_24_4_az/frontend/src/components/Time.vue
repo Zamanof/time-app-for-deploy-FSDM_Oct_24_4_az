ec10d86d9f327f5a6012ef7e2ddad01062341829
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2026-05-12 10:02:00 +0400

    docker deploy v2

--- Docker_FSDM_Oct_24_4_az/frontend/src/components/Time.vue
+++ Docker_FSDM_Oct_24_4_az/frontend/src/components/Time.vue
@@ -19,7 +19,7 @@ export default {
   },
   created: async function () {
     this.startInterval()
-    const res = await fetch('http://localhost:5000/times')
+    const res = await fetch('/times')
     const json = await res.json()
     if (json.length) this.savedTimes = json
   },
--- Docker_FSDM_Oct_24_4_az/frontend/src/utils/time.js
+++ Docker_FSDM_Oct_24_4_az/frontend/src/utils/time.js
@@ -8,7 +8,7 @@ function startInterval() {
 
 async function saveTime() {
   const time = this.currentTime
-  const res = await fetch('http://localhost:5000/times', {
+  const res = await fetch('/times', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -23,7 +23,7 @@ async function saveTime() {
 }
 
 async function deleteTime(id) {
-  const res = await fetch(`http://localhost:5000/time/${id}`, {
+  const res = await fetch(`/time/${id}`, {
     method: 'DELETE',
   })
   const json = await res.json()
